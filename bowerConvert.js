30e776ea63e4ac56994fee342d320931a0786c7b
John-David Dalton <john.david.dalton@gmail.com>
2015-09-23 08:04:44 -0700

    Add `bowerConvert` module.

---
+++ bowerConvert.js
@@ -0,0 +1,15 @@
+var baseConvert = require('./lib/baseConvert.js');
+
+/*----------------------------------------------------------------------------*/
+
+/**
+ * Converts `lodash` to an auto-curried iteratee-first data-last version.
+ *
+ * @param {Function} lodash The lodash function.
+ * @returns {Function} Returns the new converted lodash function.
+ */
+function bowerConvert(lodash) {
+  return baseConvert(lodash, lodash);
+}
+
+module.exports = bowerConvert;
